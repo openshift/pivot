a409281be3d451eacfb166076bb348ff5716a46f
Steve Milner <smilner@redhat.com>
2018-06-12 10:28:52 -0400

    cmd: Add the root command
    Signed-off-by: Steve Milner <smilner@redhat.com>

--- types/imageinspection.go
+++ types/imageinspection.go
@@ -6,6 +6,13 @@ import (
 	"github.com/opencontainers/go-digest"
 )
 
+const (
+	// PivotDonePath is the path to the file used to denote pivot work
+	PivotDonePath = "/etc/os-container-pivot.stamp"
+	// PivotName is literally the name of the new pivot
+	PivotName = "ostree-container-pivot"
+)
+
 // ImageInspection is a public implementation of
 // https://github.com/projectatomic/skopeo/blob/master/cmd/skopeo/inspect.go#L20-L31
 type ImageInspection struct {
