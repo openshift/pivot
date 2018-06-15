7f0f91ccc33076869faedff2b1752eb2b2335736
Steve Milner <smilner@redhat.com>
2018-06-15 11:39:09 -0400

    main.go: Suppress noisy log warnings
    Resolves #5
    
    Signed-off-by: Steve Milner <smilner@redhat.com>

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+
+	"github.com/spf13/pflag"
 
 	// TODO: update to official location
 	"github.com/ashcrow/pivot/cmd"
@@ -15,6 +18,10 @@ var version string
 // main is the entry point for the command
 func main() {
 	fmt.Printf("pivot version %s (%s)\n", version, commitHash)
+
+	flag.CommandLine.Parse([]string{})
+	pflag.Set("logtostderr", "true")
+
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
