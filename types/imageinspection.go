1a8dbe4e98c0990215547cf1c6326d53748d336a
Jonathan Lebon <jonathan@jlebon.com>
2018-09-12 16:04:19 -0400

    types: use permalink to skopeo spec
    Ref links are temporary, permalinks are forever. ❤

--- types/imageinspection.go
+++ types/imageinspection.go
@@ -14,7 +14,7 @@ const (
 )
 
 // ImageInspection is a public implementation of
-// https://github.com/projectatomic/skopeo/blob/master/cmd/skopeo/inspect.go#L20-L31
+// https://github.com/containers/skopeo/blob/82186b916faa9c8c70cfa922229bafe5ae024dec/cmd/skopeo/inspect.go#L20-L31
 type ImageInspection struct {
 	Name          string `json:",omitempty"`
 	Tag           string `json:",omitempty"`
