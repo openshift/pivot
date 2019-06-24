802b78814d953e83a62ee796be7af06c9b1cf21e
Antonio Murdaca <runcom@linux.com>
2019-06-24 16:35:09 +0200

    utils: fix FileExists

--- utils/fs.go
+++ utils/fs.go
@@ -8,11 +8,9 @@ import (
 
 // FileExists checks if the file exists, gracefully handling ENOENT.
 func FileExists(path string) bool {
-	if _, err := os.Stat(path); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-		glog.Fatalf("Failed to stat %s: %v", path, err)
+	if _, err := os.Stat(path); err == nil {
+		return true
 	}
-	return true
+	glog.Warningf("Failed to stat %s: %v", path, err)
+	return false
 }
